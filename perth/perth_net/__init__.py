49cd0d1e712003452267c06d20a1bba9de32807b
Ali Tavallaie <a.tavallaie@gmail.com>
2025-12-03 13:31:18 +0330

    change order of the line for importing PREPACKAGED_MODELS_DIR

--- perth/perth_net/__init__.py
+++ perth/perth_net/__init__.py
@@ -1,3 +1,4 @@
 from importlib.resources import files
-from .perth_net_implicit.perth_watermarker import PerthImplicitWatermarker
+
 PREPACKAGED_MODELS_DIR = files(__name__).joinpath("pretrained")
+from .perth_net_implicit.perth_watermarker import PerthImplicitWatermarker  # noqa: E402, F401
